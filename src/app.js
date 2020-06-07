31f9500cd7939699ddb3006a37fac853628137b5
danielazeveedo2k12 <danielazeveedo2k12@gmail.com>
2020-06-07 19:31:50 -0300

    Habilita o COR

--- src/app.js
+++ src/app.js
@@ -7,6 +7,14 @@ const app        = express();
 app.use( bodyParser.json( { limit: '5mb' } ) );
 app.use( bodyParser.urlencoded( { extended: false } ) );
 
+// Habilita o CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-backend-token");
+    next();
+});
+
 //CARREGAR ROTAS
 const routeIndex = require('./routes/index');
 const routeFilms = require('./routes/films');
